27adcd16e20400b9c17e0a3dc5629a53c36d4190
sershishkov <ser.shishkov@gmail.com>
2019-10-14 19:15:36 +0300

    12-10

--- store/actions/places.js
+++ store/actions/places.js
@@ -1,11 +1,29 @@
+import * as FileSystem from 'expo-file-system';
+
 import { ADD_PLACE } from '../types';
 
-export const addPlace = (title, imagePath) => {
-  return {
-    type: ADD_PLACE,
-    placeData: {
-      title: title,
-      image: imagePath
+export const addPlace = (title, image) => {
+  return async dispatch => {
+    const fileName = image.split('/').pop();
+
+    const newPath = FileSystem.documentDirectory + fileName;
+
+    try {
+      await FileSystem.moveAsync({
+        from: image,
+        to: newPath
+      });
+    } catch (err) {
+      console.log(err);
+      throw err;
     }
+
+    dispatch({
+      type: ADD_PLACE,
+      placeData: {
+        title: title,
+        image: newPath
+      }
+    });
   };
 };
