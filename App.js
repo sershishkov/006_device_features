9b146864b530d6cb557b01f356cdbd98726d794c
sershishkov <ser.shishkov@gmail.com>
2019-10-15 19:01:27 +0300

    12-11

--- App.js
+++ App.js
@@ -2,6 +2,15 @@ import React from 'react';
 import { Provider } from 'react-redux';
 
 import store from './store/store';
+import { init } from './helpers/db';
+
+init()
+  .then(() => {
+    console.log('Initialized database');
+  })
+  .catch(err => {
+    console.log(`Initialized db failed, error : ${err}`);
+  });
 
 import PlacesNavigator from './navigation/PlacesNavigator';
 
